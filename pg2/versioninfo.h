bbc445d3ce6ddc3b8dca8ae5edffd7bc27d078b3
peerblockproject <peerblockproject@gmail.com>
2009-09-14 02:49:16 +0000

    git-svn-id: https://peerblock.googlecode.com/svn/trunk@152 d3e0e2f6-51e0-11de-b413-c5e99dc0a8e2

--- pg2/versioninfo.h
+++ pg2/versioninfo.h
@@ -31,7 +31,7 @@
 //#define PB_RELTYPE_DEV
 
 // BUILDDATE: YYMMDDnnnn, where YY MM and DD are the year, month, and day of this build, and nnnn is for build-number
-#define BUILDDATE 908280$WCREV$	// PB_REV
+#define BUILDDATE 909120$WCREV$	// PB_REV
 
 #define PB_BLDNUM $WCREV$
 
