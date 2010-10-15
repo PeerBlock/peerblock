dac5ed3182440d7235e97ac29449ceba69575cd3
peerblockproject <peerblockproject@gmail.com>
2010-10-15 01:36:06 +0000

    1.1.x branch:  Updating versioninfo builddate for new Beta Release 1.0+ (r484).
    git-svn-id: https://peerblock.googlecode.com/svn/branches/1.1.x@484 d3e0e2f6-51e0-11de-b413-c5e99dc0a8e2

--- pb/versioninfo.h
+++ pb/versioninfo.h
@@ -31,7 +31,7 @@
 //#define PB_RELTYPE_DEV
 
 // BUILDDATE: YYMMDDnnnn, where YY MM and DD are the year, month, and day of this build, and nnnn is for build-number
-#define BUILDDATE 1006090$WCREV$	// PB_REV
+#define BUILDDATE 1010100$WCREV$	// PB_REV
 
 #define PB_VER_BUILDNUM $WCREV$
 
