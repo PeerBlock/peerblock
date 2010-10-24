447fa4b85846c872ca2b6b1bff5a485cf6b31167
XhmikosR <xhmikosr@gmail.com>
2010-10-24 12:26:45 +0000

    added copyright notice in the batch files,  fixed "|" character
    git-svn-id: https://peerblock.googlecode.com/svn/trunk@500 d3e0e2f6-51e0-11de-b413-c5e99dc0a8e2

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -1,4 +1,25 @@
 @ECHO OFF
+
+REM   PeerBlock copyright (C) 2009-2010 PeerBlock, LLC
+
+REM   This software is provided 'as-is', without any express or implied
+REM   warranty.  In no event will the authors be held liable for any damages
+REM   arising from the use of this software.
+
+REM   Permission is granted to anyone to use this software for any purpose,
+REM   including commercial applications, and to alter it and redistribute it
+REM   freely, subject to the following restrictions:
+
+REM   1. The origin of this software must not be misrepresented; you must not
+REM      claim that you wrote the original software. If you use this software
+REM      in a product, an acknowledgment in the product documentation would be
+REM      appreciated but is not required.
+REM   2. Altered source versions must be plainly marked as such, and must not be
+REM      misrepresented as being the original software.
+REM   3. This notice may not be removed or altered from any source distribution.
+
+REM   $Id$
+
 SETLOCAL
 TITLE Compiling PeerBlock...
 
@@ -11,7 +32,7 @@ IF %bool% GTR 0 (
 TITLE build.bat %1
 ECHO.
 ECHO:Usage:
-ECHO: build.bat [Clean|Build|Rebuild]
+ECHO: build.bat [Clean^|Build^|Rebuild]
 ECHO:Executing "build.bat" will use the defaults: "build.bat Rebuild"
 ECHO.
 ENDLOCAL
