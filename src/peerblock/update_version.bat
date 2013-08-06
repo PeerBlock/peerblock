f71a3a3358b3266112ba3bf1415e968ebe088aa4
XhmikosR <xhmikosr@gmail.com>
2013-08-06 04:41:18 +0000

    Guard version_parsed.h.
    git-svn-id: https://peerblock.googlecode.com/svn/trunk@637 d3e0e2f6-51e0-11de-b413-c5e99dc0a8e2

--- src/peerblock/update_version.bat
+++ src/peerblock/update_version.bat
@@ -15,7 +15,9 @@ ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
 ECHO You should (re)install TortoiseSVN.
 ECHO I'll use PB_VER_BUILDNUM=9999 for now.
 
-ECHO #define PB_VER_MAJOR 1 > version_parsed.h
+ECHO #pragma once > version_parsed.h
+ECHO. >> version_parsed.h
+ECHO #define PB_VER_MAJOR 1 >> version_parsed.h
 ECHO #define PB_VER_MINOR 1 >> version_parsed.h
 ECHO #define PB_VER_BUGFIX 0 >> version_parsed.h
 ECHO #define PB_VER_BUILDNUM 9999 >> version_parsed.h
