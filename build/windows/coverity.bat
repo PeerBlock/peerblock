71feb98b59c3abe4d555dfdc297b20146ec658ee
XhmikosR <xhmikosr@gmail.com>
2014-04-14 17:39:23 +0000

    Update coverity.bat.
    * use the new coverity version
    * use tar/lzma if available
    * auto-detect 7zip
    
    git-svn-id: https://peerblock.googlecode.com/svn/trunk@706 d3e0e2f6-51e0-11de-b413-c5e99dc0a8e2

--- build/windows/coverity.bat
+++ build/windows/coverity.bat
@@ -8,29 +8,66 @@ SETLOCAL
 
 PUSHD %~dp0
 
-SET COVDIR=H:\progs\thirdparty\cov-analysis-win64-6.6.1
+IF NOT DEFINED COVDIR SET "COVDIR=H:\progs\thirdparty\cov-analysis-win64-7.0.2"
+IF DEFINED COVDIR IF NOT EXIST "%COVDIR%" (
+  ECHO.
+  ECHO ERROR: Coverity not found in "%COVDIR%"
+  GOTO End
+)
 SET "PB_DDK_DIR=H:\WinDDK\7600.16385.1"
 
-CALL "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
+CALL "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO vcvarsall.bat call failed.
+  GOTO End
+)
+
+
+:Cleanup
+IF EXIST "cov-int" RD /q /s "cov-int"
+IF EXIST "Peerblock.lzma" DEL "Peerblock.lzma"
+IF EXIST "Peerblock.tar"  DEL "Peerblock.tar"
+IF EXIST "Peerblock.tgz"  DEL "Peerblock.tgz"
+
+
+:Main
 SET MSBUILD_SWITCHES=/nologo /consoleloggerparameters:Verbosity=minimal /maxcpucount^
  /nodeReuse:true /target:Rebuild /property:Configuration="Release_(Vista)";Platform=Win32
 
 "%COVDIR%\bin\cov-build.exe" --dir cov-int MSBuild "PeerBlock.sln" %MSBUILD_SWITCHES%
 
+
 :tar
-IF EXIST "PeerBlock.tgz" DEL "PeerBlock.tgz"
 tar --version 1>&2 2>NUL || (ECHO. & ECHO ERROR: tar not found & GOTO SevenZip)
-tar czvf PeerBlock.tgz cov-int
+tar caf "Peerblock.lzma" "cov-int"
 GOTO End
 
+
 :SevenZip
-IF NOT EXIST "%PROGRAMFILES%\7za.exe" (ECHO. & ECHO ERROR: "%PROGRAMFILES%\7za.exe" not found & GOTO End)
-IF EXIST "PeerBlock.tar" DEL "PeerBlock.tar"
-IF EXIST "PeerBlock.tgz" DEL "PeerBlock.tgz"
-"%PROGRAMFILES%\7za.exe" a -ttar PeerBlock.tar cov-int
-"%PROGRAMFILES%\7za.exe" a -tgzip PeerBlock.tgz PeerBlock.tar
-IF EXIST "PeerBlock.tar" DEL "PeerBlock.tar"
+CALL :SubDetectSevenzipPath
+
+rem Coverity is totally bogus with lzma...
+rem And since I cannot replicate the arguments with 7-Zip, just use tar/gzip.
+IF EXIST "%SEVENZIP%" (
+  "%SEVENZIP%" a -ttar "Peerblock.tar" "cov-int"
+  "%SEVENZIP%" a -tgzip "Peerblock.tgz" "Peerblock.tar"
+  IF EXIST "Peerblock.tar" DEL "Peerblock.tar"
+  GOTO End
+)
+
+
+:SubDetectSevenzipPath
+FOR %%G IN (7z.exe) DO (SET "SEVENZIP_PATH=%%~$PATH:G")
+IF EXIST "%SEVENZIP_PATH%" (SET "SEVENZIP=%SEVENZIP_PATH%" & EXIT /B)
+
+FOR %%G IN (7za.exe) DO (SET "SEVENZIP_PATH=%%~$PATH:G")
+IF EXIST "%SEVENZIP_PATH%" (SET "SEVENZIP=%SEVENZIP_PATH%" & EXIT /B)
+
+FOR /F "tokens=2*" %%A IN (
+  'REG QUERY "HKLM\SOFTWARE\7-Zip" /v "Path" 2^>NUL ^| FIND "REG_SZ" ^|^|
+   REG QUERY "HKLM\SOFTWARE\Wow6432Node\7-Zip" /v "Path" 2^>NUL ^| FIND "REG_SZ"') DO SET "SEVENZIP=%%B\7z.exe"
+EXIT /B
 
 
 :End
